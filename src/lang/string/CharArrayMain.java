cef307df0bcfb5d0794df5ca1935a8f1e86a56a1
Clariatinet <zzzqw59@gmail.com>
2026-05-23 23:16:53 +0900

    String 클래스 - 기본

---
+++ src/lang/string/CharArrayMain.java
@@ -0,0 +1,11 @@
+package lang.string;
+
+public class CharArrayMain {
+    public static void main(String[] args) {
+        char[] charArray = new char[]{'h', 'e', 'l', 'l', 'o'};
+        System.out.println(charArray);
+
+        String str = "hello";
+        System.out.println(str);
+    }
+}
---
+++ src/lang/string/equals/StringEqualsMain1.java
@@ -0,0 +1,16 @@
+package lang.string.equals;
+
+public class StringEqualsMain1 {
+    public static void main(String[] args) {
+        String str1 = new String("hello");
+        String str2 = new String("hello");
+
+        System.out.println("new String() == 비교: " + (str1 == str2));
+        System.out.println("new String() == 비교: " + (str1.equals(str2)));
+
+        String str3 = "hello";
+        String str4 = "hello";
+        System.out.println("리터럴 == 비교: " + (str3 == str4));
+        System.out.println("리터럴 == 비교: " + (str3.equals(str4)));
+    }
+}
