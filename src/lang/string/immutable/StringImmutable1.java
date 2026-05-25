236f697fd9360d046087cb85134a43a2d05abc8b
Clariatinet <zzzqw59@gmail.com>
2026-05-25 21:46:09 +0900

    String 클래스 - 예제

---
+++ src/lang/string/immutable/StringImmutable1.java
@@ -0,0 +1,9 @@
+package lang.string.immutable;
+
+public class StringImmutable1 {
+    public static void main(String[] args) {
+        String str = "hello";
+        str.concat("java");
+        System.out.println("str = " + str);
+    }
+}
